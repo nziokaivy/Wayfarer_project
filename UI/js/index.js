94b715e50305569c0249c2b05bf267a6a7773b6c
nziokaivy <nziokaivy@gmail.com>
2019-07-31 12:01:36 +0300

    [Finishes #67614040]: Add profile page feature where users can view their bookings

---
+++ UI/js/index.js
@@ -0,0 +1,87 @@
+
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+  
+  
+  function navBar() {
+     var x = document.getElementById("myTopnav");
+     if (x.className === "topnav") {
+       x.className += " responsive";
+     } else {
+       x.className = "topnav";
+     }
+  }
+  //sticky navbar
+  window.onscroll = function() {stickyNav()};
+  
+  var navbar = document.getElementById("myTopnav");
+  var sticky = navbar.offsetTop;
+  
+  // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+  function stickyNav() {
+    if (window.pageYOffset >= sticky) {
+      myTopnav.classList.add("sticky")
+    } else {
+      myTopnav.classList.remove("sticky");
+    }
+  }
+  
+  function functionAlert(msg, myYes) {
+      var confirmBox = $("#confirm");
+      confirmBox.find(".message").text(msg);
+      confirmBox.find(".yes").unbind().click(function() {
+         confirmBox.hide();
+      });
+      confirmBox.find(".yes").click(myYes);
+      confirmBox.show();
+   }
+
+
+ 
+ function navBar() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+ }
+ //sticky navbar
+ window.onscroll = function() {stickyNav()};
+ 
+ var navbar = document.getElementById("myTopnav");
+ var sticky = navbar.offsetTop;
+ 
+ // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+ function stickyNav() {
+   if (window.pageYOffset >= sticky) {
+     myTopnav.classList.add("sticky")
+   } else {
+     myTopnav.classList.remove("sticky");
+   }
+ }
+ 
+ function functionAlert(msg, myYes) {
+     var confirmBox = $("#confirm");
+     confirmBox.find(".message").text(msg);
+     confirmBox.find(".yes").unbind().click(function() {
+        confirmBox.hide();
+     });
+     confirmBox.find(".yes").click(myYes);
+     confirmBox.show();
+  }
+ 
+
+
+
