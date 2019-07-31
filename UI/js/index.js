78ce2f075498fcb02ee6e7e4191a8dc4013a1a25
nziokaivy <nziokaivy@gmail.com>
2019-07-31 14:29:35 +0300

    [Finshes #167616941]: Add user can view specific trip feature

--- UI/js/index.js
+++ UI/js/index.js
@@ -82,6 +82,20 @@ function openTab(evt, tabName) {
      confirmBox.show();
   }
  
+//FUNCTIONALITY FOR THE SPECIFIC TRIP
+function toggleFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("more-button");
 
-
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
+}
 
