0ec00f336b2d697ed5c5a2bd035021f14d7c56e3
nziokaivy <nziokaivy@gmail.com>
2019-08-01 21:01:52 +0300

    [Finishes #167651533]: Add sign in endpoint feature

--- server/api/routes/index.js
+++ server/api/routes/index.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import SignUp from '../controllers/signUpController';
+import SignIn from '../controllers/signInController';
 
 const router = express.Router();
 
 //authentication routes
 router.post('/auth/signup', SignUp.signup);
+router.post('/auth/signin', SignIn.signin);
 
 
 export default router;
