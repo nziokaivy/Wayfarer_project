176aacd832f78513e1c34f43415fa2b170f4ce6a
nziokaivy <nziokaivy@gmail.com>
2019-08-01 21:54:42 +0300

    [Finishes #167651676]: Add user can book a seat endpoint feature

--- server/api/routes/index.js
+++ server/api/routes/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import SignUp from '../controllers/signUpController';
 import SignIn from '../controllers/signInController';
 import Trip from '../controllers/tripController';
+import Booking from '../controllers/bookingController';
 
 const router = express.Router();
 
@@ -14,5 +15,8 @@ router.post('/trips', Trip.createNewTrip);
 router.get('/trips', Trip.getAllTrips);
 router.get('/trips/:id', Trip.getSpecificTrip);
 router.patch('/trips/:id/cancel', Trip.cancelTrip);
+
+//booking routes
+router.post('/bookings', Booking.booking);
 
 export default router;
---
+++ server/test/booking.test.js
@@ -0,0 +1,30 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../api/server';
+import Booking from '../api/db/booking';
+
+const should = chai.should();
+chai.use(chaiHttp);
+
+describe('Book Seat', () => {
+    // TEST FOR BOOKING A SEAT
+    it('POST/api/v1/bookings Should book a seat', (done) => {
+        const bookings = {
+            id: Booking.getAllBookings().length + 1,
+            trip_id: 1,
+            user_id: 1,
+            seat_number: 23,
+            created_on: '23-07-2019',
+
+        };
+        chai
+        .request(app)
+        .post('/api/v1/bookings')
+        .send(bookings)
+        .end((err, res) => {
+            res.should.have.status(201);
+            res.should.should.be.a('object');
+            done();
+        });
+    });
+});
