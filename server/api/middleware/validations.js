36c8442c1b14bcbfd1b196239bede082ef2468c5
nziokaivy <nziokaivy@gmail.com>
2019-08-07 17:24:46 +0300

    [Feature #167739792]: Update travis file

--- server/api/middleware/validations.js
+++ server/api/middleware/validations.js
@@ -4,9 +4,23 @@ import User from '../db/user';
 class allValidations {
     static signup(req,res,next) {
         const { first_name,last_name,email,password,} = req.body;
+        if (!first_name) {
+			return res.status(400).json({status:'error', messgage:'Please fill in your first name'});
+        }
+        if (!last_name) {
+			return res.status(400).json({status:'error', messgage:'Please fill in your last name'});
+        }
+        if (!email) {
+			return res.status(400).json({status:'error', messgage:'Please fill in your email address'});
+        }
+        if (!password) {
+			return res.status(400).json({status:'error', messgage:'Please fill in a password of your choice'});
+        }
+        
         if (!first_name || !last_name || !email || !password) {
-			return res.status(400).json({status:'error', messgage:'Ensure you have: firstname,lastname, email and password'});
-		}
+			return res.status(400).json({status:'error', messgage:'Ensure you have fill in: first name,last name, email and password'});
+        }
+
         const schema = Joi.object().keys({
             first_name: Joi.string().trim().required(),
             last_name: Joi.string().trim().required(),
