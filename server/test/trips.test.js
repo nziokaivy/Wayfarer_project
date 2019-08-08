9a67367cd05df3249dd9595e98ec0c1b2edcbb5b
nziokaivy <nziokaivy@gmail.com>
2019-08-08 21:02:46 +0300

    [Finishes #167790767]: Add more tests

--- server/test/trips.test.js
+++ server/test/trips.test.js
@@ -12,7 +12,7 @@ const adminToken = Token.genToken(1, true, 'admin@test.com', 'admin', 'test');
 const userToken = Token.genToken(2, false, 'user@test.com', 'user', 'test');
 
 describe('Trip Tests', () => {
-    // TEST FOR POST  NEW TRIP
+    // TEST FOR CREATING A NEW TRIP
     it('POST/api/v1/trips Should create a new trip', (done) => {
         const trip = {
             seating_capacity: '67',
@@ -29,6 +29,138 @@ describe('Trip Tests', () => {
             .set('authorization', `Bearer ${adminToken}`)
             .end((err, res) => {
                 res.should.have.status(201);
+                res.should.should.be.a('object');
+                done();
+            });
+    });
+
+    // TEST FOR CANNOT CREATE A NEW TRIP WITH A MISSING SEATING CAPACITY
+    it('POST/api/v1/trips Should not create a new trip if seating capacit is missing', (done) => {
+        const trip = {
+            seating_capacity: '',
+            bus_license_number: 'KZE 432',
+            origin: 'Nairobi',
+            destination: 'Kigali',
+            trip_date: '23/08/2019',
+            fare: '4000',
+        };
+        chai
+            .request(app)
+            .post('/api/v1/trips')
+            .send(trip)
+            .set('authorization', `Bearer ${adminToken}`)
+            .end((err, res) => {
+                res.should.have.status(400);
+                res.should.should.be.a('object');
+                done();
+            });
+    });
+
+    // TEST FOR CANNOT CREATE A NEW TRIP WITH A MISSING BUS LICENSE NUMBER
+    it('POST/api/v1/trips Should not create a new trip if bus license number is missing', (done) => {
+        const trip = {
+            seating_capacity: '23',
+            bus_license_number: '',
+            origin: 'Nairobi',
+            destination: 'Kigali',
+            trip_date: '23/08/2019',
+            fare: '4000',
+        };
+        chai
+            .request(app)
+            .post('/api/v1/trips')
+            .send(trip)
+            .set('authorization', `Bearer ${adminToken}`)
+            .end((err, res) => {
+                res.should.have.status(400);
+                res.should.should.be.a('object');
+                done();
+            });
+    });
+
+    // TEST FOR CANNOT CREATE A NEW TRIP WITH A MISSING ORIGIN
+    it('POST/api/v1/trips Should not create a new trip if origin is missing', (done) => {
+        const trip = {
+            seating_capacity: '23',
+            bus_license_number: 'KZE 432',
+            origin: '',
+            destination: 'Kigali',
+            trip_date: '23/08/2019',
+            fare: '4000',
+        };
+        chai
+            .request(app)
+            .post('/api/v1/trips')
+            .send(trip)
+            .set('authorization', `Bearer ${adminToken}`)
+            .end((err, res) => {
+                res.should.have.status(400);
+                res.should.should.be.a('object');
+                done();
+            });
+    });
+
+    // TEST FOR CANNOT CREATE A NEW TRIP WITH A MISSING DESTINATION
+    it('POST/api/v1/trips Should not create a new trip if destination is missing', (done) => {
+        const trip = {
+            seating_capacity: '23',
+            bus_license_number: 'KZE 432',
+            origin: 'Nairobi',
+            destination: '',
+            trip_date: '23/08/2019',
+            fare: '4000',
+        };
+        chai
+            .request(app)
+            .post('/api/v1/trips')
+            .send(trip)
+            .set('authorization', `Bearer ${adminToken}`)
+            .end((err, res) => {
+                res.should.have.status(400);
+                res.should.should.be.a('object');
+                done();
+            });
+    });
+
+    // TEST FOR CANNOT CREATE A NEW TRIP WITH A MISSING TRIP DATE
+    it('POST/api/v1/trips Should not create a new trip if trip date is missing', (done) => {
+        const trip = {
+            seating_capacity: '23',
+            bus_license_number: 'KZE 432',
+            origin: 'Nairobi',
+            destination: 'KIgali',
+            trip_date: '',
+            fare: '4000',
+        };
+        chai
+            .request(app)
+            .post('/api/v1/trips')
+            .send(trip)
+            .set('authorization', `Bearer ${adminToken}`)
+            .end((err, res) => {
+                res.should.have.status(400);
+                res.should.should.be.a('object');
+                done();
+            });
+    });
+
+    // TEST FOR CANNOT CREATE A NEW TRIP WITH A MISSING FARE
+    it('POST/api/v1/trips Should not create a new trip if fare is missing', (done) => {
+        const trip = {
+            seating_capacity: '23',
+            bus_license_number: 'KZE 432',
+            origin: 'Nairobi',
+            destination: 'KIgali',
+            trip_date: '23/08/2019',
+            fare: '',
+        };
+        chai
+            .request(app)
+            .post('/api/v1/trips')
+            .send(trip)
+            .set('authorization', `Bearer ${adminToken}`)
+            .end((err, res) => {
+                res.should.have.status(400);
                 res.should.should.be.a('object');
                 done();
             });
