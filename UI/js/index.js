5990429481f6c5dbee58b3d84eec19cd033c156e
nziokaivy <nziokaivy@gmail.com>
2019-08-11 10:37:46 +0300

    [Finishes #167826410]: Add filter by destination feature

--- UI/js/index.js
+++ UI/js/index.js
@@ -45,7 +45,7 @@ function openTab(evt, tabName) {
     }
   }
 
-  function aFunction() {
+  function originFunction() {
     var input, filter, table, tr, td, i, txtValue;
     input = document.getElementById("myInput");
     filter = input.value.toUpperCase();
@@ -53,6 +53,25 @@ function openTab(evt, tabName) {
     tr = table.getElementsByTagName("tr");
     for (i = 0; i < tr.length; i++) {
       td = tr[i].getElementsByTagName("td")[1];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }       
+    }
+  }
+
+  function destinationFunction() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("destinationInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[3];
       if (td) {
         txtValue = td.textContent || td.innerText;
         if (txtValue.toUpperCase().indexOf(filter) > -1) {
